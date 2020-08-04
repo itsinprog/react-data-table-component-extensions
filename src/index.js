6a893171915fba6d45bb2c939679b24072b201de
Lock <43507394+itsinprog@users.noreply.github.com>
2020-08-03 19:44:24 -0500

    Remove styling for next.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './index.css';
+// import './index.css';
 import { Filter, Export, Print } from './ui';
 import Utilities from './utilities';
 import ExportMethod from './export';
